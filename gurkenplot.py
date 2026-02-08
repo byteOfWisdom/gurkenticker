c4a9e60e20be5eec9f5186e4a157b5edf07e4705
Leoni <leoni.dessau@mailbox.org>
2026-02-08 15:18:45 +0100

    working date formatter

--- gurkenplot.py
+++ gurkenplot.py
@@ -1,22 +1,25 @@
 #!python3
 from matplotlib import pyplot as plt
+import matplotlib as mpl
 import numpy as np
 import std
 
 
 def parse_date(s):
     s = s.replace(".", "-")
-    return s
     return np.datetime64(s)
 
 
+@np.vectorize
 def to_delta_t(timestamp):
-    np.timedelta64(timestamp - np.datetime64("2026-01-01"), "d")
-    return timestamp
+    dt = timestamp - np.datetime64("1970-01-01")
+    return int(dt / np.timedelta64(1, "D"))
 
-def format_date(epoch_timestamp, _) -> str:
-    # return strftime('%Y-%m-%d', localtime(epoch_timestamp))
-    return str(epoch_timestamp)
+
+@np.vectorize
+def format_date(time_days, _) -> str:
+    start = np.datetime64("1970-01-01")
+    return np.datetime_as_string(start + np.timedelta64(time_days, "D"))
 
 
 def parse_csv(fname):
@@ -25,11 +28,18 @@ def parse_csv(fname):
     prices = [float(line.split(";")[1]) for line in content]
     return dates, prices
 
+
 def main():
     dates, prices = parse_csv("./gurken.csv")
     std.default.plt_pretty("Datum", "Gurkenpreis / €")
-    plt.scatter(dates, prices)
-    plt.gca().xaxis.set_major_formatter(format_date)
+    days = to_delta_t(dates)
+    print(days)
+    for d in days:
+        print(format_date(d, None))
+    plt.scatter(days, prices)
+    formatter = mpl.dates.DateFormatter('%d-%m-%Y')
+    plt.gca().xaxis.set_major_formatter(formatter)
+    # plt.gca().xaxis.set_major_formatter(format_date)
     plt.show()
     
 
